89e8f9d113952d761823f67912d8d48ef57d5b80
JaminCO <76646536+JaminCO@users.noreply.github.com>
2021-05-22 19:09:41 +0100

    Add files via upload

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+	SECRET_KEY = 'fbf21f273c2b6864d2d3ee1e9ad6014f'
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	MAIL_SERVER = 'smtp.gmail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('EMAIL_USER')
+	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
